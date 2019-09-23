ae21504d51a2b94f037574b164fce4b8086fb593
greenpeppers100 <evanwitulski@gmail.com>
2019-09-23 12:53:41 -0700

    Progress!

--- cpe123/lab1_example.js
+++ cpe123/lab1_example.js
@@ -6,13 +6,29 @@ function setup() {
 
 let degrees = 0;
 function draw() {
+	let dolphinColor =('#5BA5FF');
+
 	background(3, 206, 252);
 
-	//Dolphin Tail
 	translate(50, 350);
-	rotate(-45);
-	ellipse(0, 0, 40, 100);
 
+	push();
+		fill(dolphinColor);
+		rotate(-45);
+		ellipse(0, 0, 40, 100);
+	pop();
 
+	push();
+		fill(dolphinColor);
+		rotate(-55);
+		ellipse(50, 0, 100, 50,);
+	pop();
 
+	//Origin Dot! Remove!
+	fill(255, 0, 0);
+	ellipse(0, 0, 5, 5);
+
+	//Reference Dot! Remove!
+	fill(255, 0, 0);
+	ellipse(65,-85,5,5);
 }
