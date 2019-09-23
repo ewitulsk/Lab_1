8678ab13dc1c47b5e20f8a8e0ed3256d11aacd86
greenpeppers100 <evanwitulski@gmail.com>
2019-09-23 10:45:48 -0700

    Made Rotate Work!

--- cpe123/lab1_example.js
+++ cpe123/lab1_example.js
@@ -1,15 +1,18 @@
 function setup() {
 	createCanvas(400, 400);
 	ellipseMode(CENTER);
+	angleMode(DEGREES);
 }
 
 let degrees = 0;
 function draw() {
-
 	background(3, 206, 252);
-	rotate(degrees);
-	//translate(200, 200);
-	ellipse(0, 0, 50, 100);
-	degrees = degrees + .05;
+
+	//Dolphin Tail
+	translate(50, 350);
+	rotate(-45);
+	ellipse(0, 0, 40, 100);
+
+
 
 }
