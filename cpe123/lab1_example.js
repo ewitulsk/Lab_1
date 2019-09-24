bca178d727e01ca4556603a79773ac2be1a1b099
greenpeppers100 <evanwitulski@gmail.com>
2019-09-23 23:12:37 -0700

    Actually drew a dolphin!

--- cpe123/lab1_example.js
+++ cpe123/lab1_example.js
@@ -10,25 +10,81 @@ function draw() {
 
 	background(3, 206, 252);
 
+
+//The Actual Dolphin
+	noStroke();
 	translate(50, 350);
+	rotate(-45);
 
+	//Body Pieces
 	push();
+
+		//Tail
 		fill(dolphinColor);
-		rotate(-45);
 		ellipse(0, 0, 40, 100);
-	pop();
 
-	push();
+		//Body Piece 1
 		fill(dolphinColor);
-		rotate(-55);
-		ellipse(50, 0, 100, 50,);
+		rotate(-10);
+		ellipse(50, 0, 100, 50);
+
+		//Reference Dot
+	//	push();
+	//		fill(255, 0, 0);
+	//		ellipse(50, 25, 5, 5);
+	//	pop();
+
+		//Body Piece 2
+		translate(50,0);
+		rotate(10);
+		ellipse(50, 0, 125, 65);
+
+		//Reference Dot 2
+	//	push();
+	//		fill(255, 0, 0);
+	//		ellipse(50, 32.5, 5, 5);
+	//	pop();
+
+		//Body Piece 3
+		translate(100,0);
+		rotate(20);
+		ellipse(50, 0, 150, 80);
+
+		//Top Fin
+	//	push();
+	//		fill(255,0,0);
+	//		ellipse(50,-40,5,5);
+	//	pop();
+		arc(50,-40,50,50, -210, -90);
+
+		//Bottom Fin
+	//	push();
+	//		fill(255,0,0);
+	//		ellipse(50,40,5,5);
+	//	pop();
+		arc(50,40,50,50, 210, 100);
+
+		//Body Piece 4
+		translate(100,0);
+		rotate(30);
+		ellipse(50, 0, 125, 80);
+
+		//Nose
+		ellipse(112.5, 0, 25,10);
+
+		//Eye
+		push();
+			fill(0,0,0);
+			ellipse(80, -15, 7, 7);
+		pop();
+
+
+
 	pop();
 
 	//Origin Dot! Remove!
-	fill(255, 0, 0);
-	ellipse(0, 0, 5, 5);
+	//fill(255, 0, 0);
+	//ellipse(0, 0, 5, 5);
 
-	//Reference Dot! Remove!
-	fill(255, 0, 0);
-	ellipse(65,-85,5,5);
+
 }
